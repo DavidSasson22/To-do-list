89a2b8c2ccbee975d4dd84f4e0e7a43b830d7594
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-03-11 09:23:51 +0200

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,7 +1,5 @@
-const firstLoadUpdate = () => {
+const dataUpdate = () => {
   for (let i = 0; i < myList.tasks.length; i++) {
-    console.log(myList.tasks.length);
-
     const item = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
@@ -10,7 +8,6 @@ const firstLoadUpdate = () => {
       item.classList.add("completed");
       item.classList.remove("uncompleted");
       checkbox.checked = true;
-
     }
     else {
       item.classList.add("uncompleted");
@@ -19,9 +16,7 @@ const firstLoadUpdate = () => {
 
     checkbox.addEventListener("click", (e) => {
       myList.tasks[i].completed = e.target.checked;
-
       localStorage.setItem("todos", JSON.stringify(myList));
-
       if (myList.tasks[i].completed) {
         item.classList.add("completed");
         item.classList.remove("uncompleted");
@@ -31,101 +26,7 @@ const firstLoadUpdate = () => {
         myList.delete(i);
         myList.tasks.push(temp);
         localStorage.setItem("todos", JSON.stringify(myList));
-      }
-      else {
-        item.classList.add("uncompleted");
-        item.classList.remove("completed");
-        checkbox.checked = myList.tasks[i].completed;
-        myList.tasks[i].yet;
-        myList.tasks[i].update;
-        localStorage.setItem("todos", JSON.stringify(myList));
-      }
-    });
-
-    const text = document.createElement("p");
-    text.innerText = myList.tasks[i].task;
-
-    item.addEventListener("click", () => {
-      // make item editable
-      text.setAttribute("contenteditable", "true");
-      text.focus();
-
-      // update upon Enter key press
-      text.addEventListener("keydown", (event) => {
-        if (event.keyCode === 13) {
-          myList.tasks[i].task = (text.innerText);
-          text.blur();
-          localStorage.setItem("todos", JSON.stringify(myList));
-
-        }
-      });
-    });
-
-    const button = document.createElement("button");
-    const trash = document.createElement("img");
-    trash.setAttribute("class", `trash`);
-    trash.setAttribute("src", `./fonts/trash-alt-regular.svg`);
-
-    button.appendChild(trash);
-
-    button.addEventListener("click", () => {
-      myList.delete(i);
-      localStorage.setItem("todos", JSON.stringify(myList));
-      render();
-    });
-
-    item.appendChild(checkbox);
-    item.appendChild(text);
-    item.appendChild(button);
-    todo.appendChild(item);
-    input.value = null;
-  }
-}
-
-
-const render = () => {
-  console.log(`render activated`);
-  todo.innerHTML = null;
-
-
-  const todos = localStorage.getItem("todos");
-  myUpList = JSON.parse(todos) || [];
-  myList.tasks = myUpList.tasks;
-
-  for (let i = 0; i < myList.tasks.length; i++) {
-    console.log(myList.tasks.length);
-
-    const item = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    if (myList.tasks[i].completed) {
-      item.classList.add("completed");
-      item.classList.remove("uncompleted");
-      checkbox.checked = true;
-
-    }
-    else {
-      item.classList.add("uncompleted");
-      item.classList.remove("completed");
-    }
-
-    checkbox.addEventListener("click", (e) => {
-      myList.tasks[i].completed = e.target.checked;
-
-      localStorage.setItem("todos", JSON.stringify(myList));
-
-      if (myList.tasks[i].completed) {
-        item.classList.add("completed");
-        item.classList.remove("uncompleted");
-        checkbox.checked = myList.tasks[i].completed;
-        myList.tasks[i].done;
-        let temp = myList.tasks[i];
-        myList.delete(i);
-        myList.tasks.push(temp);
-        localStorage.setItem("todos", JSON.stringify(myList));
-        render()
-
+        makePage()
       }
       else {
         item.classList.add("uncompleted");
@@ -136,43 +37,33 @@ const render = () => {
         myList.delete(i);
         myList.tasks.unshift(temp);
         localStorage.setItem("todos", JSON.stringify(myList));
-        render()
+        makePage()
       }
     });
 
     const text = document.createElement("p");
     text.innerText = myList.tasks[i].task;
-
     item.addEventListener("click", () => {
-      // make item editable
       text.setAttribute("contenteditable", "true");
       text.focus();
-
-      // update upon Enter key press
       text.addEventListener("keydown", (event) => {
         if (event.keyCode === 13) {
           myList.tasks[i].task = (text.innerText);
           text.blur();
           localStorage.setItem("todos", JSON.stringify(myList));
-
         }
       });
-
-
     });
 
     const button = document.createElement("button");
     const trash = document.createElement("img");
     trash.setAttribute("class", `trash`);
     trash.setAttribute("src", `./fonts/trash-alt-regular.svg`);
-
     button.appendChild(trash);
-
-
     button.addEventListener("click", () => {
       myList.delete(i);
       localStorage.setItem("todos", JSON.stringify(myList));
-      render();
+      makePage();
     });
 
     item.appendChild(checkbox);
@@ -184,20 +75,31 @@ const render = () => {
 }
 
 
+const makePage = () => {
+  todo.innerHTML = null;
+
+  const todos = localStorage.getItem("todos");
+  myUpList = JSON.parse(todos) || [];
+  myList.tasks = myUpList.tasks;
+
+  dataUpdate();
+}
+
+
 const addTodo = (value) => {
   if (value) {
     let newTodo = new Task;
     newTodo.task = value;
     myList.create(newTodo);
     localStorage.setItem("todos", JSON.stringify(myList));
-    render();
+    makePage();
   }
 }
 
 
 const main = () => {
   makeNewList();
-  firstLoadUpdate();
+  dataUpdate();
   const form = document.querySelector("#form");
   const input = document.querySelector("#input");
 
@@ -208,9 +110,7 @@ const main = () => {
 }
 
 let todos = localStorage.getItem("todos");
-console.log(todos);
 todos = JSON.parse(todos);
-console.log(todos);
 
 
 const myList = new ToDoList;
